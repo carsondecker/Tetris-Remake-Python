70c678a18c6ed18effc41ff163cf7c5c20a954e0
CarsonDecker24 <carsondecker1205@gmail.com>
2024-11-16 20:24:00 -0600

    drew next queue and fixed handling

--- Game/main.py
+++ Game/main.py
@@ -28,23 +28,24 @@ class Game:
         self.lines_cleared = 0
         
         self.clock = pygame.time.Clock()
-        self.drop_speed = 50
+        self.gravity = 50 # In 'G', where 1G is moving down 1 cell per frame
         self.last_drop_time = pygame.time.get_ticks()
         self.lock_delay = 500  # Time in ms before piece locks
         self.last_move_time = 0
         self.lock_time = 0
         
-        # DAS
-        self.das_delay = 170
-        self.das_speed = 50
+        # Handling
+        self.das = 125
+        self.arr = 0
         self.last_key_down_time = 0
         self.moving_direction = 0
+        self.reset_das = False
         
         self.fill_next_queue()
 
     def fill_next_queue(self):
         if len(self.next_pieces) <= 7:
-            bag = list(PIECES.keys())
+            bag = list(map(lambda x: Tetromino(x), PIECES.keys()))
             random.shuffle(bag)
             self.next_pieces.extend(bag)
         if not self.current_piece:
@@ -52,7 +53,7 @@ class Game:
 
     def spawn_piece(self):
         if not self.current_piece:
-            self.current_piece = Tetromino(self.next_pieces.pop(0))
+            self.current_piece = self.next_pieces.pop(0)
             self.fill_next_queue()
         
         if self.board.check_collision(self.current_piece):
@@ -94,21 +95,30 @@ class Game:
     def handle_das(self):
         if self.moving_direction != 0:
             current_time = pygame.time.get_ticks()
-            if current_time - self.last_key_down_time >= self.das_delay:
-                if current_time - self.last_move_time >= self.das_speed:
+
+            if self.reset_das:
+                self.last_key_down_time = current_time
+                self.reset_das = False
+
+            if current_time - self.last_key_down_time >= self.das:
+                if current_time - self.last_move_time >= self.arr:
                     self.current_piece.move(self.board, self.moving_direction, 0)
                     self.last_move_time = current_time
+
+    def draw_preview_piece(self, piece, starting_x, starting_y, width, height):
+        piece.draw(self.screen, starting_x + ((width - len(piece.shape[0]) * CELL_SIZE) // 2), 
+                   starting_y + ((height - (3 if piece.piece_name == 'I' else 2) * CELL_SIZE) // 2), True)
 
     def draw_hold(self):
         pygame.draw.rect(self.screen, BOARD_BORDER, (BOARD_OFFSET_X - SIDEBAR_OFFSET - SIDEBAR_WIDTH, BOARD_OFFSET_Y, SIDEBAR_WIDTH, PREVIEW_PIECE_HEIGHT), 1)
         if self.held_piece:
             self.draw_preview_piece(self.held_piece, BOARD_OFFSET_X - SIDEBAR_OFFSET - SIDEBAR_WIDTH, BOARD_OFFSET_Y, SIDEBAR_WIDTH, PREVIEW_PIECE_HEIGHT)
 
-    def draw_preview_piece(self, piece, starting_x, starting_y, width, height):
-        print(starting_y + ((height - (1 if self.held_piece.piece_name == 'I' else 2) * CELL_SIZE) // 2))
-        piece.draw(self.screen, starting_x + ((width - len(self.held_piece.shape[0]) * CELL_SIZE) // 2), 
-                   starting_y + ((height - (3 if self.held_piece.piece_name == 'I' else 2) * CELL_SIZE) // 2), True)
-
+    def draw_next_queue(self):
+        pygame.draw.rect(self.screen, BOARD_BORDER, (BOARD_OFFSET_X + BOARD_WIDTH * CELL_SIZE + SIDEBAR_OFFSET, BOARD_OFFSET_Y, SIDEBAR_WIDTH, PREVIEW_PIECE_HEIGHT * 5), 1)
+        for i in range(5):
+            self.draw_preview_piece(self.next_pieces[i], BOARD_OFFSET_X + (BOARD_WIDTH * CELL_SIZE + 1) + SIDEBAR_OFFSET,
+                                     BOARD_OFFSET_Y + PREVIEW_PIECE_HEIGHT * i, SIDEBAR_WIDTH, PREVIEW_PIECE_HEIGHT)
 
     def draw(self):
         self.screen.fill(BLACK)
@@ -117,6 +127,7 @@ class Game:
         self.current_piece.draw(self.screen)
         self.current_piece.draw_ghost(self.screen, self.ghost_surface, self.board)
         self.draw_hold()
+        self.draw_next_queue()
     
         pygame.display.flip()
 
@@ -131,12 +142,16 @@ class Game:
                     
                     if not self.paused:
                         if event.key == pygame.K_LEFT:
+                            if self.moving_direction == 1:
+                                self.reset_das = True
                             self.moving_direction = -1
                             self.last_key_down_time = pygame.time.get_ticks()
                             if self.current_piece:
                                 self.current_piece.move(self.board, -1, 0)
                         
                         elif event.key == pygame.K_RIGHT:
+                            if self.moving_direction == -1:
+                                self.reset_das = True
                             self.moving_direction = 1
                             self.last_key_down_time = pygame.time.get_ticks()
                             if self.current_piece:
@@ -160,7 +175,9 @@ class Game:
             
             elif event.type == pygame.KEYUP:
                 if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                    self.moving_direction = 0
+                    if (event.key == pygame.K_LEFT and self.moving_direction == -1) or \
+                       (event.key == pygame.K_RIGHT and self.moving_direction == 1):
+                        self.moving_direction = 0
     
     def update(self):
         while self.running:
@@ -173,7 +190,7 @@ class Game:
                 self.handle_das()
                 
                 # Handle piece dropping
-                if current_time - self.last_drop_time > self.drop_speed:
+                if current_time - self.last_drop_time > self.gravity:
                     if self.current_piece:
                         if not self.board.check_collision(self.current_piece, 0, 1):
                             self.current_piece.move(self.board, 0, 1)
